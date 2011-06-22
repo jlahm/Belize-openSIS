90b4f7e2c0ee71ea630a2d35c5af87c4f49cc581
jlahm development site <jlahmdev@jlahm.com>
2011-06-22 14:40:05 -0400

    modified:   data.php

--- data.php
+++ data.php
@@ -1,8 +1,8 @@
 <?php 
 $DatabaseType = 'mysql'; 
 $DatabaseServer = 'localhost'; 
-$DatabaseUsername = 'jlahm'; 
-$DatabasePassword = 'J@me$@llenL@hm1951'; 
+$DatabaseUsername = 'opensis'; 
+$DatabasePassword = 'B3liz30p3n$i$'; 
 $DatabaseName = 'opensis'; 
 $DatabasePort = '3306'; 
 $DefaultSyear = '2010'; 
